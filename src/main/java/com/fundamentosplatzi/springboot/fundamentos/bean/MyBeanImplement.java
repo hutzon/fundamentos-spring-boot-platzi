812877ff2e5e0f32f20ab9391de5e99495b25988
Ottoniel Campos <hutzon4011@gmail.com>
2022-01-07 12:54:38 -0600

    Segundo commit

---
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/bean/MyBeanImplement.java
@@ -0,0 +1,10 @@
+package com.fundamentosplatzi.springboot.fundamentos.bean;
+
+public class MyBeanImplement implements MyBean{
+
+
+    @Override
+    public void print() {
+        System.out.println("Hola desde mi implementeacion propia del bean");
+    }
+}
